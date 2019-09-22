ae8c4e685c566fd125f6ab3e48e9ef4442140f29
Jenna Pederson <jenna@612softwarefoundry.com>
2019-09-22 10:36:15 -0500

    Add header nav and clean up index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,29 @@
 <template>
   <div>
+    <nav class="navbar is-spaced" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">
+          Starter for Nuxt Markdown Blog
+        </nuxt-link>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="header-navbar-menu">
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+
+      <div id="header-navbar-menu" class="navbar-menu">
+        <div class="navbar-end">
+          <nuxt-link to="/" class="navbar-item">
+            Home
+          </nuxt-link>
+        </div>
+      </div>
+    </nav>
     <nuxt />
   </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
