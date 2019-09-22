6b22119645a7925e01a480231bf1a950254b371b
Jenna Pederson <jenna@612softwarefoundry.com>
2019-09-22 18:22:49 -0500

    Wire up hamburger button for mobile; Change eslint settings to not be so ridiculous

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,19 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 3,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": true
+      }
+    }],
+    "vue/html-indent": ["error", 2, {
+      "alignAttributesVertically": false,
+    }]
   }
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,14 +6,20 @@
           Starter for Nuxt Markdown Blog
         </nuxt-link>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="header-navbar-menu">
+        <a role="button"
+          :class="{'is-active': showHeaderNavbarMenu}"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="header-navbar-menu"
+          @click="toggleHeaderNavbarMenu">
           <span aria-hidden="true" />
           <span aria-hidden="true" />
           <span aria-hidden="true" />
         </a>
       </div>
 
-      <div id="header-navbar-menu" class="navbar-menu">
+      <div id="header-navbar-menu" class="navbar-menu" :class="{'is-active': showHeaderNavbarMenu}">
         <div class="navbar-end">
           <nuxt-link to="/" class="navbar-item">
             Home
@@ -24,6 +30,21 @@
     <nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      showHeaderNavbarMenu: false
+    }
+  },
+  methods: {
+    toggleHeaderNavbarMenu () {
+      this.showHeaderNavbarMenu = !this.showHeaderNavbarMenu
+    }
+  }
+}
+</script>
 
 <style>
 </style>
