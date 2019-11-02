3b0900dcb015f459858ee478c1c99a3217e0ad04
Jenna Pederson <jenna@612softwarefoundry.com>
2019-11-02 14:54:14 -0500

    Load env vars with dotenv and send email with mailgun-js

--- functions/send-contact-email/send-contact-email.js
+++ functions/send-contact-email/send-contact-email.js
@@ -1,15 +1,30 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+require('dotenv').config()
+const { MAILGUN_API_KEY, MAILGUN_DOMAIN, MAILGUN_URL, FROM_EMAIL_ADDRESS, CONTACT_TO_EMAIL_ADDRESS } = process.env
+const mailgun = require('mailgun-js')({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN, url: MAILGUN_URL })
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed', headers: { 'Allow': 'POST' } }
   }
+
+  const data = JSON.parse(event.body)
+  if (!data.message || !data.contactName || !data.contactEmail) {
+    return { statusCode: 422, body: 'Name, email, and message are required.' }
+  }
+
+  const mailgunData = {
+    from: FROM_EMAIL_ADDRESS,
+    to: CONTACT_TO_EMAIL_ADDRESS,
+    'h:Reply-To': data.contactEmail,
+    subject: `New contact from ${data.contactName}`,
+    text: `Name: ${data.contactName}\nEmail: ${data.contactEmail}\nMessage: ${data.message}`
+  }
+
+  return mailgun.messages().send(mailgunData).then(() => ({
+    statusCode: 200,
+    body: "Your message was sent successfully! We'll be in touch."
+  })).catch(error => ({
+    statusCode: 422,
+    body: `Error: ${error}`
+  }))
 }
